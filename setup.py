ec4c2b2f39c85850ae584a5929d95eda8ddcdce1
Eric Wong <eric.wong.t@gmail.com>
2013-09-26 20:48:42 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='rumrunner',
-    version='0.0.1',
+    version='0.0.2',
     description='Client to send metrics to Speakeasy server',
     author='Eric Wong',
     py_modules=['rumrunner'],
