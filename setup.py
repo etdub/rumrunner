4325e084ed697dfaff688cf6acfcf0a8c045f1a7
Eric Wong <eric.wong.t@gmail.com>
2013-09-26 20:47:42 +0000

    don't need a package. just the module.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='rumrunner',
     version='0.0.1',
     description='Client to send metrics to Speakeasy server',
     author='Eric Wong',
-    packages=['rumrunner'],
+    py_modules=['rumrunner'],
     )
