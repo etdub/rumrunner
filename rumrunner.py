48712b0e0dee6856d46a07942d15d15b9c03325a
Eric Wong <eric.wong.t@gmail.com>
2013-10-04 00:48:11 +0000

    Move to 4 spaces. Add percentile metric type.

--- rumrunner.py
+++ rumrunner.py
@@ -3,34 +3,36 @@ import ujson
 import zmq
 
 class Rumrunner(object):
-  def __init__(self, metric_socket, app_name):
-    self.metric_socket = metric_socket
-    self.app_name = app_name
+    def __init__(self, metric_socket, app_name):
+        self.metric_socket = metric_socket
+        self.app_name = app_name
 
-    self.context = zmq.Context()
+        self.context = zmq.Context()
 
-    # Send metrics
-    self.send_socket = self.context.socket(zmq.PUSH)
-    self.send_socket.connect('ipc://{0}'.format(self.metric_socket))
-    self.send_socket.setsockopt(zmq.LINGER, 0)
+        # Send metrics
+        self.send_socket = self.context.socket(zmq.PUSH)
+        self.send_socket.connect('ipc://{0}'.format(self.metric_socket))
+        self.send_socket.setsockopt(zmq.LINGER, 0)
 
-  def counter(self, metric_name, value=1):
+    def counter(self, metric_name, value=1):
+        self.send(metric_name, value, 'COUNTER')
 
-    try:
-      self.send_socket.send(ujson.dumps([self.app_name, metric_name, 'COUNTER',  value]), zmq.NOBLOCK)
-    except zmq.error.Again:
-      pass
+    def gauge(self, metric_name, value):
+        self.send(metric_name, value, 'GAUGE')
 
-  def gauge(self, metric_name, value):
-    try:
-      self.send_socket.send(ujson.dumps([self.app_name, metric_name, 'GAUGE', value]), zmq.NOBLOCK)
-    except zmq.error.Again:
-      pass
+    def percentile(self, metric_name, value):
+        self.send(metric_name, value, 'PERCENTILE')
+
+    def send(self, metric_name, value, metric_type):
+        try:
+            self.send_socket.send(ujson.dumps([self.app_name, metric_name, metric_type,  value]), zmq.NOBLOCK)
+        except zmq.error.Again:
+            pass
 
 if __name__ == '__main__':
-  m = Rumrunner('/var/tmp/metric_socket', 'test.app')
-  for x in range(1000):
-    if x % 100 == 0:
-      print x
-    m.counter('test_counter', 1)
-    m.gauge('test_gauge', x)
+    m = Rumrunner('/var/tmp/metric_socket', 'test.app')
+    for x in range(1000):
+        if x % 100 == 0:
+            print x
+        m.counter('test_counter', 1)
+        m.gauge('test_gauge', x)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='rumrunner',
-    version='0.0.2',
+    version='0.0.3',
     description='Client to send metrics to Speakeasy server',
     author='Eric Wong',
     py_modules=['rumrunner'],
