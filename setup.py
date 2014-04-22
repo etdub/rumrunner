cb650dc06a6dbb368c320ddf6dbc58699e5ec83f
Eric Wong <eric.wong.t@gmail.com>
2014-04-22 02:19:07 +0000

    Add install requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
 setup(name='rumrunner',
-    version='0.0.4',
+    version='0.0.5',
     description='Client to send metrics to Speakeasy server',
     author='Eric Wong',
     py_modules=['rumrunner'],
+    install_requires = [
+        'pyzmq',
+        'ujson',
+    ]
     )
