68dd14779107f3703cd9874610220a786335a5bc
Qingping Hou <qihou@linkedin.com>
2014-12-19 18:15:32 -0800

    test: add unit tests

---
+++ test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+import random
+import os
+
+import zmq
+
+from rumrunner import Rumrunner
+
+
+class TestRumrunner(unittest.TestCase):
+
+    def test_error_out_on_not_writable_socket(self):
+        ctx = zmq.Context()
+        recv_socket = ctx.socket(zmq.PULL)
+        tmp_metric_socket = '/var/tmp/test_metric_{0}'.format(random.random())
+        recv_socket.bind('ipc://{0}'.format(tmp_metric_socket))
+
+        Rumrunner(tmp_metric_socket, 'test_app')
+
+        os.chmod(tmp_metric_socket, 0444)
+        self.assertRaises(Exception, Rumrunner, tmp_metric_socket, 'test_app')
+
+        os.remove(tmp_metric_socket)
+
+
+if __name__ == '__main__':
+        unittest.main()
